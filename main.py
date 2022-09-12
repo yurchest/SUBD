caecae8f92fa6dc7ba5d0c3121ce2f5f1cc03b0d
Yuriy <yurchest@email.com>
2022-09-13 00:45:57 +0300

    add ui

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-def main():
-    print("hello!!")
-main()
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from form import *
+
+
+class App(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.w = QtWidgets.QMainWindow()
+        self.w_root = Ui_MainWindow()
+        self.w_root.setupUi(self.w)
+
+        self.w.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = App()
+    app.exec()
