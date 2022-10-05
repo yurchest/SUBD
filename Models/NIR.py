53abac0264d5c7124b7a1534f16bfb554a39e329
Yuriy <yurchest@email.com>
2022-10-06 02:07:11 +0300

    commit ^_^

--- Models/NIR.py
+++ Models/NIR.py
@@ -3,15 +3,15 @@ from PyQt6.QtCore import Qt
 
 
 class NirModel(QSqlTableModel):
-    header_data = {'codvuz': "Код вуза",
-                   "rnw": "Рег. номер НИР",
-                   "f1": "Характер  НИР",
-                   "z2": "Сокр. наим. вуза",
+    header_data = {'codvuz': "Код \nвуза",
+                   "rnw": "Регистрационный \nномер НИР",
+                   "f1": "Характер  \nНИР",
+                   "z2": "Сокращенное \nнаим. вуза",
                    "f6": "Руководитель НИР",
                    "f10": "Код темы по ГРНТИ",
                    "f2": "Наименование НИР",
-                   "f7": "Должность руководителя",
-                   "f18": "Плановый объем финансирования",
+                   "f7": "Должность \nруководителя",
+                   "f18": "Плановый объем \nфинансирования",
                    }
 
     def __init__(self, db):
@@ -22,9 +22,12 @@ class NirModel(QSqlTableModel):
         self.setTable("Tp_nir")
         self.change_column_name()
         self.select()
-        # self.setEditStrategy(QSqlTableModel.EditStrategy.OnManualSubmit)
 
     def change_column_name(self):
         for i in range(self.columnCount()):
             self.setHeaderData(i, Qt.Orientation.Horizontal, self.header_data[self.record().fieldName(i)])
+
+    def update(self):
+        query = QSqlQuery("SELECT * FROM Tp_nir")
+        self.setQuery(query)
 
--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys
 import os
 from dotenv import load_dotenv
 
-from PyQt6.QtSql import QSqlDatabase
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox, QAbstractItemView
 from UI.py_ui.form import *
 from Models.NIR import NirModel
+from PyQt6.QtCore import Qt
 
 from Widgets.edit_record_nir import EditRecordNir
 
@@ -30,27 +31,35 @@ class App(QWidget):
         self.w = QtWidgets.QMainWindow()
         self.w_root = Ui_MainWindow()
         self.w_root.setupUi(self.w)
-        self.db = connect_db(os.getenv('DB_URL'))
 
-        nir_model = NirModel(self.db)
-        self.w_root.tableView.setModel(nir_model)
-        self.init_tableview_nir()
+        self.db = connect_db(os.getenv('DB_URL'))
+        self.nir_model = NirModel(self.db)
+        self.w_root.tableView.setModel(self.nir_model)
+        self.update_nir_view()
 
         self.edit_record_nir_form = None
 
         self.w_root.pushButton.clicked.connect(self.open_edit_record_nir)
+        self.w_root.pushButton_3.clicked.connect(self.delete_records_nir)
 
         self.w.show()
 
-    def init_tableview_nir(self):
+    def update_nir_view(self):
         self.w_root.tableView.setSortingEnabled(True)
-        self.w_root.tableView.horizontalHeader().moveSection(0, 1)
+        self.w_root.tableView.horizontalHeader().moveSection(3, 0)
+        self.w_root.tableView.horizontalHeader().moveSection(4, 1)
+        self.w_root.tableView.horizontalHeader().moveSection(8, 2)
+        self.w_root.tableView.horizontalHeader().moveSection(6, 3)
+        self.w_root.tableView.horizontalHeader().moveSection(7, 4)
+        self.w_root.tableView.horizontalHeader().moveSection(8, 5)
+        self.w_root.tableView.horizontalHeader().setFixedHeight(60)
         self.resize_columns_nir()
         self.w_root.tableView.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
 
     def resize_columns_nir(self):
-        for i in [0, 1, 2, 3, 4, 5, 7, 8]:
+        for i in range(9):
             self.w_root.tableView.resizeColumnToContents(i)
+            self.w_root.tableView.setColumnWidth(i, self.w_root.tableView.columnWidth(i) - 10)
         self.w_root.tableView.setColumnWidth(6, 200)
 
     def open_edit_record_nir(self):
@@ -61,6 +70,30 @@ class App(QWidget):
             self.edit_record_nir_form.close()
             self.edit_record_nir_form = None
             self.open_edit_record_nir()
+
+    def delete_records_nir(self):
+        def accept_delete(rows_to_delete):
+            for row in rows_to_delete:
+                query = QSqlQuery(f"DELETE FROM TP_nir WHERE codvuz={row['codvuz']} AND rnw='{row['rnw']}'")
+                # qury2 = QSqlQuery()
+                # qury2.exec(f"SELECT * FROM TP_nir WHERE codvuz={row['codvuz']} and rnw='{row['rnw']}'")
+                # while qury2.next():
+                #     print(qury2.value(0))
+                self.nir_model.setQuery(query)
+            self.nir_model.update()
+            self.update_nir_view()
+            # self.w_root.tableView.setModel(self.nir_model)
+
+        rows_to_delete = []
+        for index in self.w_root.tableView.selectionModel().selectedRows():
+            rows_to_delete.append(
+                {'codvuz': self.nir_model.data(self.nir_model.index(index.row(), 0)),
+                 'rnw': self.nir_model.data(self.nir_model.index(index.row(), 1))
+                 })
+        if rows_to_delete:
+            # TODO Подтверждение удаления
+            accept_delete(rows_to_delete)
+
 
 
 if __name__ == "__main__":
